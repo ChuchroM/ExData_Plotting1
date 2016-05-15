aa1f33c89b25ee56dbdf05944944d04ac1e42510
ChuchroM <chuchro@geol.agh.edu.pl>
2016-05-15 19:38:43 +0200

    Update plot2.R

--- plot2.R
+++ plot2.R
@@ -1,6 +1,10 @@
+
+## reading and subseting data in R.file with first plot
 ##linear plot of Global Active Power, with x label in days, save in plot2.png file
 
 png(filename="plot2.png", height=480, width=480, 
-    bg="white", col="black")
-plot(data1$date.time, data1$Global_active_power, type="l", xlab=" ", ylab="Global Active Power (kilowatts)")
-dev.off()
+    bg="white")
+Sys.setlocale("LC_ALL", "English")
+
+plot(data1$Global_active_power~data1$Datetime, type="l", ylab="Global Active Power (kilowatts)", xlab="")
+dev.off()
